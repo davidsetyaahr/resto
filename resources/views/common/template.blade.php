55e9370c6da3f24726c2c5ce6f66866c57063016
davidsetyaahr <44167895+davidsetyaahr@users.noreply.github.com>
2020-06-23 16:19:28 +0700

    Update template.blade.php

--- resources/views/common/template.blade.php
+++ resources/views/common/template.blade.php
@@ -21,7 +21,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
   <meta name="author" content="Creative Tim">
-  <title>{{$pageInfo}} - Baratha Coffee</title>
+  <title>{{isset($pageInfo) ? $pageInfo.' - ' : ''}}Baratha Coffee</title>
   <!-- Favicon -->
   <link rel="icon" href="{{ asset('assets/img/coffee.png') }}" type="image/png">
   <!-- Fonts -->
