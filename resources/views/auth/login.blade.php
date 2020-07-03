e16dfca738b8d2a91fe013a79322603be7b20fb0
Inant <inant.kharisma@gmail.com>
2020-07-03 10:28:54 +0700

    login with username

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -59,9 +59,9 @@
                     <div class="input-group-prepend">
                       <span class="input-group-text"><i class="ni ni-user-run"></i></span>
                     </div>
-                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                    <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
 
-                    @error('email')
+                    @error('username')
                         <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
                         </span>
